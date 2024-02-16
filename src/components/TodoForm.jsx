309e7a26ced447108c7c930273e53b1e2e8a36d3
Fardin <fardin.mz01@gmail.com>
2024-02-16 23:39:40 +0330

    pass data Todoform to TodoApp (Child to parent)

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ onAddTodo }) => {
   const [tern, setTern] = useState("");
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddTodo(tern);
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <input value={tern} onChange={({ target }) => setTern(target.value)} />
-      <button type="submit">Save</button>
+    <form className="p-2 border-b border-gray-400 " onSubmit={handleSubmit}>
+      <input
+        className="border px-2 py-1 mr-2 rounded-md"
+        value={tern}
+        onChange={({ target }) => setTern(target.value)}
+      />
+      <button
+        className="bg-green-400 py-1 px-4 border-green-500 border text-white rounded-md hover:bg-green-700 transition-colors"
+        type="submit "
+      >
+        Save
+      </button>
     </form>
   );
 };
