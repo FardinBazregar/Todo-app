b1ae8f216eb99b0b28bd28088e6c328e773c1311
Fardin <fardin.mz01@gmail.com>
2024-02-20 19:56:02 +0330

    create button for items

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,11 @@ export const TodoItem = ({ title, done }) => {
   return (
     <div className=" flex justify-between bg-white shadow-md rounded-md mb-4 p-4">
       <p className={clsx({ "line-through": done })}>{title}</p>
+
+      <div>
+        <button className="px-1 mx-1">✅</button>
+        <button className="px-1 mx-1">❌</button>
+      </div>
     </div>
   );
 };
