b4853fc45157c2806babea1c8402feaa4c1a3bc5
Fardin <fardin.mz01@gmail.com>
2024-02-22 11:13:45 +0330

    create local Storage for items

--- src/components/hooks/useTodoApp.js
+++ src/components/hooks/useTodoApp.js
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 export const useTodoApp = () => {
-  const [defaulList, { get }] = useLocalStorage("todos");
-  const [list, setList] = useState(defaulList);
+  const [defaulList, { set }] = useLocalStorage("todos");
+  const [list, setList] = useState(defaulList || []);
 
   const handleDeletItem = (todoId) => {
     const newList = list.filter(({ id }) => id !== todoId);
+    set("todos", newList);
+
     setList(newList);
   };
   const handleToggleItem = (Todoid) => {
-    setList(
-      list.map((item) => {
-        if (item.id === Todoid) item.done = !item.done;
-        return item;
-      })
-    );
+    const todoItems = list.map((item) => {
+      if (item.id === Todoid) item.done = !item.done;
+      return item;
+    });
+    setList(todoItems);
+    set("todos", todoItems);
   };
 
   const handleTodoAdd = (title) => {
@@ -23,7 +25,9 @@ export const useTodoApp = () => {
       title,
       done: false,
     };
-    setList([...list, newTodoItem]);
+    const newTodos = [...list, newTodoItem];
+    setList(newTodos);
+    set("todos", newTodos);
   };
 
   return {
