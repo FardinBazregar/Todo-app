7d6ba00759ee2122349a90d50f6fd728948b4e99
Fardin <fardin.mz01@gmail.com>
2024-02-22 10:57:28 +0330

    get localstorage

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,1 +1,19 @@
-export const useLocalStorage = () => {};
+export const useLocalStorage = ({ key }) => {
+  const set = (key, value) => {
+    let data;
+    if (typeof value === "object") {
+      data = JSON.stringify(value);
+    }
+    localStorage.setItem(key, data);
+  };
+
+  const get = (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  };
+
+  return {
+    set,
+    get,
+    remove,
+  };
+};
